cc45490a0df85788b5b4411d031fd40659e124cb
Michael Jansen <mjansen4857@gmail.com>
2024-01-21 20:36:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 # Install pre-reqs
+sudo apt update
 sudo apt install -y python3-pip
 sudo apt install -y --no-install-recommends gstreamer1.0-gl gstreamer1.0-opencv gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-tools libgstreamer-plugins-base1.0-dev libgstreamer1.0-0 libgstreamer1.0-dev
 
